62fbeba4dde6ad7fc110c1e0a5e4b357f34543ca
ibrahimaliyevv2 <7cauc3b@code.edu.az>
2023-08-13 13:23:20 +0400

    added

---
+++ ChatOnlyJava/src/main/java/chatonlyjava/ChatOnlyJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package chatonlyjava;
+
+/**
+ *
+ * @author Ibrahim Aliyev
+ */
+public class ChatOnlyJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
